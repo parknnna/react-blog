9b4615e507eb828c573a1c65d19557f77d54d62e
parknnna <af777888@naver.com>
2021-03-10 23:25:23 +0900

    포트 수정

--- src/main/java/com/parknnna/blog/config/WebConfig.java
+++ src/main/java/com/parknnna/blog/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.parknnna.blog.config;
 
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost:8080")
+        registry.addMapping("/**").allowedOrigins("http://localhost:81","http://localhost:8080")
         .allowedMethods("*");
-
+        registry.addMapping("/*").allowedOrigins("http://localhost:81","http://localhost:8080")
+        .allowedMethods("*");
+        // InetAddress local;
+        // String ip;
+		// try { local = InetAddress.getLocalHost(); 
+		// 	ip = local.getHostAddress(); 
+		// 	System.out.println("local ip : "+ip);
+        //     registry.addMapping("/**").allowedOrigins("http://"+ip+":81","http://"+ip+":8080")
+        // .allowedMethods("*");
+		// }catch (UnknownHostException e1) { e1.printStackTrace(); }
     }
 }
